3e60734d32aad884afd8503a7d56bc922b3bd8a0
Nikolas De Giorgis <nikolas.degiorgis@iit.it>
2018-12-04 16:38:20 +0100

    new stringet features

--- stringent.h
+++ stringent.h
@@ -1,8 +1,9 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <algorithm>
 #include <iostream>
-
+#include <map>
 namespace stringent
 {
   decltype(auto) explode(const  std::string& s, char c)
@@ -18,4 +19,20 @@ namespace stringent
     while(pos!=std::string::npos);
     return res;
   }
+
+  decltype(auto) occurrences(const std::string& s, char c)
+    {
+      return std::count_if(s.begin(),s.end(), [c](char e){return c==e;});
+    }
+
+  decltype(auto) occurrences(const std::string& s)
+    {
+      std::map<char,unsigned int> occurr;
+      for (auto & ch: s)
+        {
+          if (occurr.find(ch)==occurr.end())
+            occurr[ch]=occurrences(s,ch);
+        }
+      return occurr;
+    }
 }
