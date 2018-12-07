99daf72afcd7df94747831d8c847a126e553de7a
Nikolas De Giorgis <nikolas.degiorgis@iit.it>
2018-12-07 11:58:46 +0100

    testing

--- manualTesting.cpp
+++ manualTesting.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 int main()
 {
-  auto  a=std::string("1 2 3");
+  auto  a=std::string("1,2 3&&4");
 
-  for(auto i: stringent::explode<>(a,' ',[](string s){return std::stoi(s);}))
-    int b=i;
+  for(auto i: stringent::explode<>(a,", &",[](string s){return (s);}))
+    cout <<i << endl;
 }
